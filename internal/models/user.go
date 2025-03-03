a34718dcce038e425f985955bb995dd18dc70e10
Мукам <mukamusmanow2003@gmail.com>
2025-03-03 05:29:45 +0300

    onnx-go

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+	Email     string    `json:"email" gorm:"unique"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
